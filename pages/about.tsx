25bca6817beb7fccddd493f6c0c67da36666d197
mike connor <mike@rocketnumber9.org>
2020-10-27 17:26:57 -0500

    next Link routing -web
    tested on web

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
 // @generated: @expo/next-adapter@2.1.41
 import React from 'react';
+import Link from 'next/link'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <Link href='./'>go home </Link>
     </View>
   );
 }
